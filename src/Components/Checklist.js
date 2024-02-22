7cc1145a80cf3413b2737edc9522dc92345eb903
nathanezra <ezrarodrigue@gmail.com>
2024-02-22 11:37:32 +0200

    css improve

--- src/Components/Checklist.js
+++ src/Components/Checklist.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import getCountry from '../Constants/Countries'
+import arrowUp from '../Images/icons8-arrow-up-50.png'
+import arrowDown from '../Images/icons8-arrow-down-50.png'
 
 const Checklist = (props) => {
 
@@ -21,18 +23,21 @@ const Checklist = (props) => {
 
     return (
         <div className='checklistContainer'>
-            {Object.entries(checkedGroups).map(([tax, group]) => (
+            {Object.entries(checkedGroups).map(([tax, group], i) => (
                 <div key={tax}>
                     <div className='row' onClick = {() => onDisplay(tax)} >
                         <img className='flag' src={getCountry(taxes, tax).img} />
                         <label className='taxLabel'>
                             <input
+                                id={`$${tax}-${i}`}
                                 type="checkbox"
                                 checked={group.checked}
                                 onChange={() => onCheckGroup(tax)}
                             />
+                            <label for={`$${tax}-${i}`}></label>
                             {tax}
                         </label>
+                        <img className='arrowUpDown' src={displayGroupUsers[tax] ? arrowUp : arrowDown} />
                     </div>
                     {displayGroupUsers[tax] ? 
                             group.grants.map((grant, index) => (
@@ -40,10 +45,12 @@ const Checklist = (props) => {
                                     <div>
                                         <label className='grantLabel'>
                                             <input
+                                                id={`${grant.name}-${tax}-${index}`}
                                                 type="checkbox"
                                                 checked={grant.checked && group.checked}
                                                 onChange={() => onCheckGrant(tax, grant.name)}
                                             />
+                                            <label for={`${grant.name}-${tax}-${index}`}></label>
                                             {grant.name}
                                         </label>
                                     </div>
